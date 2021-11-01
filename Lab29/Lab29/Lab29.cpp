5ff1b377b68173970775b816c538876dabbcc9be
irchen4 <81490453+irchen4@users.noreply.github.com>
2021-11-01 13:03:59 +0300

    Lab29

---
+++ Lab29/Lab29/Lab29.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	float a, b, c;
+	cout << "\na = "; cin >> a;
+	cout << "\nb = "; cin >> b;
+	cout << "\nc = "; cin >> c;
+	if(a == b && a == c) cout << "\n Треугольник равносторонний";
+	else cout << "\n Треугольник не равносторонний.";
+}
